dff2d6b911d5545e8630f4a7c5c224c9b2250bc1
fwxks <mrpcmn@protonmail.com>
2019-04-08 21:54:39 -0300

    Stopwords cleaner and other updates

--- src/tweets-processor/cleaner/cleaner.py
+++ src/tweets-processor/cleaner/cleaner.py
@@ -1,3 +1,4 @@
+from nltk.corpus import stopwords
 import nltk
 import re
 
@@ -6,9 +7,9 @@ class Broom:
     def __init__(self, language):
         self._rt_regex = re.compile('RT\s@\w{1,}:\s')
         self._hashtag_regex = re.compile('\s{1,}#\w{1,}\s{1,}')
-        self._no_alpha_regex = re.compile('[^\w]{1,}')
+        self._no_alpha_regex = re.compile(r'[!\"#$¿¡*%&\'\(\)*+,-./:;<=>?@\[\\\]^_`\{|\}~]')
         self.stopwords = set()
-        #self.initializer(language)
+        self.initializer(language)
 
     def clean_rt(self, line):
         return re.sub(self._rt_regex, ' ', line)
@@ -19,11 +20,14 @@ class Broom:
     def clean_no_alpha(self, line):
         return re.sub(self._no_alpha_regex, ' ', line)
 
+    def clean_stopwords(self, line):
+        return " ".join((word for word in line.split() if word not in self.stopwords))
+
     def cleaner(self, line):
         clean_data = self.clean_no_alpha(self.clean_hashtag(self.clean_rt(line)))
-        return clean_data
+        return self.clean_stopwords(clean_data) if len(self.stopwords) == 0 else clean_data
 
     def initializer(self, language):
         if nltk.download('stopwords'):
-            self.stopwords.update(corpus.stopwords.words(language))
+            self.stopwords.update(stopwords.words(language))
 
